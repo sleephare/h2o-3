b392e4770f8f321cd90a28476c6f6a1de8e02518
mmalohlava <michal@h2o.ai>
2017-05-29 18:28:42 -0700

    XGBoost tests for OSX (no gpu) running.
    - single node tests enabled
    - multi node tests disabled (needs mateusz commit)
    
    - fix path

--- h2o-extensions/xgboost/src/main/java/hex/api/xgboost/RegisterRestApi.java
+++ h2o-extensions/xgboost/src/main/java/hex/api/xgboost/RegisterRestApi.java
@@ -3,7 +3,7 @@ package hex.api.xgboost;
 import hex.tree.xgboost.XGBoost;
 import water.api.SchemaServer;
 
-public class RegisterApi extends water.api.AbstractRegister {
+public class RegisterRestApi extends water.api.AbstractRegister {
 
   @Override
   public void register(String relativeResourcePath) {
