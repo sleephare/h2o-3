36cc0de86cb8a709be8f103227694c5938eec2b4
wendycwong <wendycwong@yahoo.com>
2017-05-30 16:13:14 -0700

    PUBDEV-3624: top/bottom N.  Adding Junit test and script to generate dataset.

--- h2o-core/src/test/java/water/rapids/ast/prims/reducers/AstTopNTest.java
+++ h2o-core/src/test/java/water/rapids/ast/prims/reducers/AstTopNTest.java
@@ -1,18 +1,24 @@
 package water.rapids.ast.prims.reducers;
 
+import hex.SplitFrame;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import water.DKV;
+import water.Key;
+import water.Scope;
 import water.TestUtil;
 import water.fvec.Frame;
 import water.rapids.Rapids;
 import water.rapids.Val;
-import water.util.ArrayUtils;
-
 
 /**
  * Test the AstTopN.java class
  */
 public class AstTopNTest extends TestUtil {
+    static Frame _train;    // store training data
+    static Frame _answerTop;        // store top 20% of data in _train
+    static Frame _answerBottom;     // store bottom 20% of data in _train
+
     @BeforeClass public static void setup() {   // randomly generate a frame here.
         stall_till_cloudsize(1);
     }
@@ -20,23 +26,87 @@ public class AstTopNTest extends TestUtil {
     //--------------------------------------------------------------------------------------------------------------------
     // Tests
     //--------------------------------------------------------------------------------------------------------------------
-    /** Test written by Nidhi to test that NaOmit actaully remove the rows with NAs in them. */
-    @Test public void TestTopN() {
-        Frame f = null;
-        Frame fNew = null;
-        try {
-            f = ArrayUtils.frame(ar("A", "B"), ard(1.0, Double.NaN), ard(2.0, 23.3),
-                    ard(3.0, 3.3), ard(Double.NaN, 3.2), ard(34.3, 2.3), ard(2.1, 4.4),
-                    ard(9.9, -1.3), ard(11.2, 2.34), ard(9.123, 2.321), ard(10.2, 87.2),
-                    ard(1.123, 2.421), ard(10.21, 2.432));
-            String x = "(topn "+ f._key+ " 1 20 0)";
-            Val res = Rapids.exec(x);         // make the call the remove NAs in frame
-            fNew = res.getFrame();            // get frame that contains top/bottom N elements
+    /** Loading in a dataset containing data from -1000000 to 1000000 multiplied by 1.1 as the float column in column 1.
+     * The other column (column 0) is a long data type with maximum data value at 2^63. */
+    @Test public void TestTopBottomN() {
+        Scope.enter();
+        int[] checkPercent = {3};//2};//, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+        int testPercent = 0;      // store test percentage
 
+        // load in the datasets with the answers
+        _train = parse_test_file(Key.make("topbottom"), "smalldata/jira/TopBottomN.csv.zip");
+        _answerTop = parse_test_file(Key.make("top20"), "smalldata/jira/Top20Per.csv.zip");
+        _answerBottom = parse_test_file(Key.make("bottom20"), "smalldata/jira/Bottom20Per.csv.zip");
+
+        Scope.track(_train);
+        Scope.track(_answerBottom);
+        Scope.track(_answerTop);
+
+        try {
+            for (int index = 0; index < checkPercent.length; index++) {
+                testPercent = checkPercent[index];
+                testTopBottom(_answerTop, testPercent, 0);  // test top %
+                testTopBottom(_answerBottom, testPercent, 1); // test bottom %
+            }
         } finally {
-            if (f != null) f.delete();
-            if (fNew != null) fNew.delete();
+            Scope.exit();
         }
     }
 
+    public static void testTopBottom(Frame answerF, int testPercent, int getBottom) {
+        Scope.enter();
+        Scope.track(answerF);
+        Frame topBN = null;
+        Frame topBL = null;
+        Frame topBNF = null;
+        Frame topBF = null;
+        Frame answerFN = null;
+        Frame answerLN = null;
+
+        try {
+
+            String x = "(topn " + _train._key + " 0 " + testPercent + " " + getBottom + ")";
+            Val res = Rapids.exec(x);         // make the call to grab top/bottom N percent
+            topBN = res.getFrame();            // get frame that contains top N elements
+            answerLN = answerF.subframe(0,1);
+            topBL = topBN.subframe(1,2);
+            Scope.track(topBN);
+            Scope.track(answerLN);
+            Scope.track(topBL);
+            checkTopBottomN(answerLN, topBL, testPercent);
+
+            x = "(topn " + _train._key + " 1 " + testPercent + " 0)";       // get top % float
+            res = Rapids.exec(x);         // make the call to grab top/bottom N percent
+            topBNF = res.getFrame();// get frame that contains top N elements
+            topBF = topBNF.subframe(1, 2);
+            answerFN = answerF.subframe(1, 2);
+            checkTopBottomN(answerFN, topBF, testPercent);
+            Scope.track(topBF);
+            Scope.track(answerFN);
+            Scope.track(topBNF);
+        } finally {
+            Scope.exit();
+        }
+    }
+    /*
+    Helper function to compare test frame result with correct answer
+     */
+    public static void checkTopBottomN(Frame answerF, Frame grabF, int nPercent) {
+        Scope.enter();
+        try {
+            double tolerance = 1e-12;
+            double nfrac = nPercent * 0.01;   // translate percentage to actual fraction
+            SplitFrame sf = new SplitFrame(answerF, new double[]{nfrac, 1 - nfrac}, new Key[]{
+                    Key.make("topN.hex"), Key.make("bottomN.hex")});
+            // Invoke the job
+            sf.exec().get();
+            Key[] ksplits = sf._destination_frames;
+            Frame topN = DKV.get(ksplits[0]).get();
+            isIdenticalUpToRelTolerance(topN, grabF, tolerance);
+            Scope.track(topN);
+            Scope.track_generic(ksplits[1].get());
+        } finally {
+            Scope.exit();
+        }
+    }
 }
--- scripts/gen_numerical_data.py
+++ scripts/gen_numerical_data.py
@@ -1,7 +1,7 @@
 import sys
-sys.path.insert(1,"../../")
+#sys.path.insert(1,"../../")
 import h2o
-from tests import pyunit_utils
+#from tests import pyunit_utils
 from random import randint
 from random import uniform
 from random import shuffle
@@ -12,10 +12,56 @@ def gen_data():
     floatA = []
     intA = []
     sizeMat = range(0,64)   # use to generate data of values 0, +/- 2^0 to +/1 2^64
+    lowBoundF = -100000
+    upperBoundF = -1*lowBoundF      # 2 million rows
+    upperBoundL = pow(2,35)
+    lowBoundL = upperBoundL-100000
     numZeros = 0
     numNans = 0   # generate Nans
     numInfs = 500
+    numRep = 4      # number of times to repeat array
+    csvFile = "/Users/wendycwong/temp/TopBottomNRep4.csv"
+    fMult = 1.1
 
+    genStaticData(intA, floatA, upperBoundL, lowBoundF, upperBoundF, fMult)
+    # save the correct sequence before shuffling for comparison purpose
+    bottom20FrameL = h2o.H2OFrame(python_obj=zip(intA[0:int(round(len(intA)*0.2))]))
+    bottom20FrameF = h2o.H2OFrame(python_obj=zip(floatA[0:int(round(len(floatA)*0.2))]))
+    h2o.download_csv(bottom20FrameL.cbind(bottom20FrameF), "/Users/wendycwong/temp/Bottom20Per.csv" )
+
+    # save the correct sequence before shuffling for comparison purpose
+    bottom20FrameL = h2o.H2OFrame(python_obj=zip(intA[int(round(len(intA)*0.8)):int(len(intA))]))
+    bottom20FrameF = h2o.H2OFrame(python_obj=zip(floatA[int(round(len(intA)*0.8)):int(len(intA))]))
+    h2o.download_csv(bottom20FrameL.cbind(bottom20FrameF), "/Users/wendycwong/temp/Top20Per.csv" )
+    # repeat the columns a few times to seriously test the algo with duplicated data.
+
+    for val in range(0, numRep):
+        intA.extend(intA)
+        floatA.extend(floatA)
+
+    shuffle(intA)   # randomly shuffle the indices
+    shuffle(floatA) #
+
+    intFrame = h2o.H2OFrame(python_obj=zip(intA))
+    floatFrame = h2o.H2OFrame(python_obj=zip(floatA))
+    h2o.download_csv(intFrame.cbind(floatFrame), csvFile)
+
+
+def genDataFrame(sizeMat, lowBound, uppderBound, numRep, numZeros, numNans, numInfs):
+    '''
+    This function will generate an H2OFrame of two columns.  One column will be float and the other will
+    be long.
+    
+    :param sizeMat: integer denoting size of bounds
+    :param lowBound: lower bound
+    :param uppderBound: 
+    :param trueRandom: 
+    :param numRep: number of times to repeat arrays in order to generate duplicated rows
+    :param numZeros: 
+    :param numNans: 
+    :param numInfs: 
+    :return: 
+    '''
     if (numNans > 0):
         floatA = [float('NaN')]*numNans
         intA = [float('NaN')]*numNans
@@ -51,27 +97,41 @@ def gen_data():
         floatA.append(uniform(tempInt, tempIntN))
         floatA.append(-1.0*uniform(tempInt, tempIntN))
 
+def genRandomData(intA, floatA, sizeMat):
+    # next generate +/- integers or various magnitude
+    for rad in sizeMat:
+        tempInt = pow(2,rad)
+        tempIntN = pow(2,rad+1)
+        intA.append(tempInt)
+        intA.append(-1*tempInt)
+        randInt = randint(tempInt, tempIntN)    # randomly generated integer, add both +/- values
+        intA.append(randInt)
+        intA.append(-1*randInt)
+        intA.append(randint(tempInt, tempIntN))     # randomly generated integer
+        intA.append(-1*(randint(tempInt, tempIntN)))    # randomly generated negative integer
 
-    # repeat the columns a few times to seriously test the algo with duplicated data.
-    intA.extend(intA)
-    intA.extend(intA)
-    intA.extend(intA)
-    shuffle(intA)   # randomly shuffle the indices
+        floatA.append(tempInt*1.0)
+        floatA.append(-1.0*tempInt)
+        tempD = uniform(tempInt, tempIntN)
+        floatA.append(tempD)
+        floatA.append(-1.0*tempD)
+        floatA.append(uniform(tempInt, tempIntN))
+        floatA.append(-1.0*uniform(tempInt, tempIntN))
 
-    floatA.extend(floatA)
-    floatA.extend(floatA)
-    floatA.extend(floatA)
-    shuffle(floatA) #
+def genStaticData(intA, floatA, upperBoundL, lowBoundF, upperBoundF, fMult):
+    for val in range(lowBoundF, upperBoundF):
+        floatA.append(val*fMult)
+        intA.append(upperBoundL)
+        upperBoundL=upperBoundL-1
 
-    intFrame = h2o.H2OFrame(python_obj=intA)
-    floatFrame = h2o.H2OFrame(python_obj=floatA)
-    finalFrame = intFrame.concat([intFrame, floatFrame])
-
-
-
+def main(argv):
+    h2o.init(strict_version_check=False)
+    gen_data()
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(gen_data)
-else:
-    gen_data()
+    main(sys.argv)
+# if __name__ == "__main__":
+#     pyunit_utils.standalone_test(gen_data)
+# else:
+#     gen_data()
 
