cd8f6dd7b9d16f1a48011f00821b19c25367e6e4
wendycwong <wendycwong@yahoo.com>
2017-05-30 18:01:37 -0700

    PUBDEV-3624: top/bottom N.  Fixing Junit.

--- h2o-core/src/test/java/water/rapids/ast/prims/reducers/AstTopNTest.java
+++ h2o-core/src/test/java/water/rapids/ast/prims/reducers/AstTopNTest.java
@@ -11,13 +11,16 @@ import water.fvec.Frame;
 import water.rapids.Rapids;
 import water.rapids.Val;
 
+import java.util.Random;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Test the AstTopN.java class
  */
 public class AstTopNTest extends TestUtil {
     static Frame _train;    // store training data
-    static Frame _answerTop;        // store top 20% of data in _train
-    static Frame _answerBottom;     // store bottom 20% of data in _train
+    public Random _rand = new Random();
 
     @BeforeClass public static void setup() {   // randomly generate a frame here.
         stall_till_cloudsize(1);
@@ -30,57 +33,50 @@ public class AstTopNTest extends TestUtil {
      * The other column (column 0) is a long data type with maximum data value at 2^63. */
     @Test public void TestTopBottomN() {
         Scope.enter();
-        int[] checkPercent = {2,3};//2};//, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+        int[] checkPercent = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19}; // complete test
+        int numRuns = 4;
         int testPercent = 0;      // store test percentage
+        Frame topLong = null, topFloat=null, bottomLong=null, bottomFloat=null;
 
         // load in the datasets with the answers
         _train = parse_test_file(Key.make("topbottom"), "smalldata/jira/TopBottomN.csv.zip");
-        _answerTop = parse_test_file(Key.make("top20"), "smalldata/jira/Top20Per.csv.zip");
-        _answerBottom = parse_test_file(Key.make("bottom20"), "smalldata/jira/Bottom20Per.csv.zip");
+        topFloat = parse_test_file(Key.make("top20"), "smalldata/jira/Top20Per.csv.zip");
+        topLong = topFloat.extractFrame(0,1);
+        bottomFloat = parse_test_file(Key.make("bottom20"), "smalldata/jira/Bottom20Per.csv.zip");
+        bottomLong = bottomFloat.extractFrame(0,1);
 
         Scope.track(_train);
-        Scope.track(_answerBottom);
-        Scope.track(_answerTop);
+        Scope.track(topLong);
+        Scope.track(topFloat);
+        Scope.track(bottomFloat);
+        Scope.track(bottomLong);
 
         try {
-            for (int index = 0; index < checkPercent.length; index++) {
+            for (int index = 0; index < numRuns; index++) { // randomly choose 4 percentages to test
+               // testPercent = checkPercent[_rand.nextInt(checkPercent.length)];
                 testPercent = checkPercent[index];
-                testTopBottom(_answerTop, testPercent, 0);  // test top %
-             //   testTopBottom(_answerBottom, testPercent, 1); // test bottom %
+                testTopBottom(topLong, testPercent, 0, "0", 0);  // test top % Long
+                testTopBottom(topFloat, testPercent, 0, "1", 1e-24);  // test top % Float
+                testTopBottom(bottomLong, testPercent, 1, "0", 0);  // test bottom % Long
+                testTopBottom(bottomFloat, testPercent, 1, "1", 1e-24);  // test bottom % Float
             }
         } finally {
             Scope.exit();
         }
     }
 
-    public static void testTopBottom(Frame answerF, int testPercent, int getBottom) {
+    public static void testTopBottom(Frame topBottom, int testPercent, int getBottom, String columnIndex,
+                                     double tolerance) {
         Scope.enter();
-        Frame topBN = null;
-        Frame topBL = null;
-        Frame topBNF = null;
-        Frame topBF = null;
-        Frame answerFN = null;
-        Frame answerLN = null;
+        Frame topBN = null, topBL = null;
         try {
-            String x = "(topn " + _train._key + " 0 " + testPercent + " " + getBottom + ")";
+            String x = "(topn " + _train._key + " "+ columnIndex + " " +testPercent + " " + getBottom + ")";
             Val res = Rapids.exec(x);         // make the call to grab top/bottom N percent
             topBN = res.getFrame();            // get frame that contains top N elements
             Scope.track(topBN);
-            answerLN = answerF.extractFrame(0,1);
-            Scope.track(answerLN);
             topBL = topBN.extractFrame(1,2);
             Scope.track(topBL);
-            checkTopBottomN(answerLN, topBL, testPercent);
-
-/*            x = "(topn " + _train._key + " 1 " + testPercent + " 0)";       // get top % float
-            res = Rapids.exec(x);         // make the call to grab top/bottom N percent
-            topBNF = res.getFrame();// get frame that contains top N elements
-            topBF = topBNF.extractFrame(1, 2);
-            answerFN = answerF.extractFrame(1, 2);
-            checkTopBottomN(answerFN, topBF, testPercent);
-            Scope.track(topBF);
-            Scope.track(answerFN);
-            Scope.track(topBNF);*/
+            checkTopBottomN(topBottom, topBL, testPercent, tolerance);
         } finally {
             Scope.exit();
         }
@@ -88,18 +84,17 @@ public class AstTopNTest extends TestUtil {
     /*
     Helper function to compare test frame result with correct answer
      */
-    public static void checkTopBottomN(Frame answerF, Frame grabF, int nPercent) {
+    public static void checkTopBottomN(Frame answerF, Frame grabF, int nPercent, double tolerance) {
         Scope.enter();
         try {
-            double tolerance = 1e-12;
-            double nfrac = nPercent * 0.01;   // translate percentage to actual fraction
+            double nfrac = 1.0*grabF.numRows()/answerF.numRows();   // translate percentage to actual fraction
             SplitFrame sf = new SplitFrame(answerF, new double[]{nfrac, 1 - nfrac}, new Key[]{
                     Key.make("topN.hex"), Key.make("bottomN.hex")});
             // Invoke the job
             sf.exec().get();
             Key[] ksplits = sf._destination_frames;
             Frame topN = DKV.get(ksplits[0]).get();
-            isIdenticalUpToRelTolerance(topN, grabF, tolerance);
+            assertTrue(isIdenticalUpToRelTolerance(topN, grabF, tolerance));
             Scope.track(topN);
             Scope.track_generic(ksplits[1].get());
         } finally {
